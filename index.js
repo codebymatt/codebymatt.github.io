34ce1b1d5debd292499c2713e1ff6c59205a03a5
codebymatt <mattcraig365@gmail.com>
2017-11-02 16:26:00 +0000

    Add description to project cards

---
+++ index.js
@@ -0,0 +1,59 @@
+function mouseOverFeedr() {
+    var div = document.getElementById("feedr-overlay");
+    div.style.visibility = "visible";
+}
+
+function mouseOutFeedr() {
+    var div = document.getElementById("feedr-overlay");
+    div.style.visibility = "hidden";
+}
+
+function toggleFeedrVisibility() {
+    var div = document.getElementById("feedr-overlay");
+    div.style.visibility = ((div.style.visibility == "visible") ? "hidden" : "visible");
+}
+
+function mouseOverTakemyspot() {
+    var div = document.getElementById("takemyspot-overlay");
+    div.style.visibility = "visible";
+}
+
+function mouseOutTakemyspot() {
+    var div = document.getElementById("takemyspot-overlay");
+    div.style.visibility = "hidden";
+}
+
+function toggleTakemyspotVisibility() {
+    var div = document.getElementById("takemyspot-overlay");
+    div.style.visibility = ((div.style.visibility == "visible") ? "hidden" : "visible");
+}
+
+function mouseOverNewton() {
+    var div = document.getElementById("newton-overlay");
+    div.style.visibility = "visible";
+}
+
+function mouseOutNewton() {
+    var div = document.getElementById("newton-overlay");
+    div.style.visibility = "hidden";
+}
+
+function toggleNewtonVisibility() {
+    var div = document.getElementById("newton-overlay");
+    div.style.visibility = ((div.style.visibility == "visible") ? "hidden" : "visible");
+}
+
+function mouseOverRonnMapps() {
+    var div = document.getElementById("ronnmapps-overlay");
+    div.style.visibility = "visible";
+}
+
+function mouseOutRonnMapps() {
+    var div = document.getElementById("ronnmapps-overlay");
+    div.style.visibility = "hidden";
+}
+
+function toggleRonnMappsVisibility() {
+    var div = document.getElementById("ronnmapps-overlay");
+    div.style.visibility = ((div.style.visibility == "visible") ? "hidden" : "visible");
+}
